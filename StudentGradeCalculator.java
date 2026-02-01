b1bc7469b15f481002b0eb37b2e708d51025d7cf
Mounika611 <rlmounika45@gmail.com>
2026-02-01 12:21:42 +0530

    Add files via upload

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = sc.nextInt();
+
+        int[] marks = new int[numSubjects];
+        int totalMarks = 0;
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + (i + 1) + ": ");
+            marks[i] = sc.nextInt();
+            if (marks[i] < 0 || marks[i] > 100) {
+                System.out.println("Invalid marks! Enter marks between 0 and 100.");
+                i--; 
+                continue;
+            }
+            totalMarks += marks[i];
+        }
+        double average = (double) totalMarks / numSubjects;
+        char grade;
+        if (average >= 90) {
+            grade = 'A';
+        } else if (average >= 80) {
+            grade = 'B';
+        } else if (average >= 70) {
+            grade = 'C';
+        } else if (average >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+        System.out.println("\n----- Student Grade Report -----");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + average + "%");
+        System.out.println("Grade: " + grade);
+ sc.close();
+    }
+}
